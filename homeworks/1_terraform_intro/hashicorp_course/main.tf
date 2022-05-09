5955fb4cdbac03b01b68389990433421b1910b5f
Oleh Kyshkevych <okyshkevych@scalhive.com>
2022-05-09 18:20:35 +0300

    Change infrastructure in GCP with adding new resources and updating it

--- homeworks/1_terraform_intro/hashicorp_course/main.tf
+++ homeworks/1_terraform_intro/hashicorp_course/main.tf
@@ -18,3 +18,38 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_firewall" "default" {
+  name    = "terraform-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8000", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  tags = ["web", "dev"]
+}
