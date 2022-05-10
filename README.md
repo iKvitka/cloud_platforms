cedc003f5bfb699b8de2a4dc8c5de3d4e969ff1a
Oleh Kyshkevych <okyshkevych@scalhive.com>
2022-05-10 12:03:34 +0300

    Define input variables

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cloud Platforms Course Homeworks
 This is a repository for homeworks from [Cloud Computing Course](https://github.com/v1r7u/cloud_computing_course)
+
+# variables for learning
+project `terraform-learning-349712`
+credentials_file `~/GCP/secrets/terraform_learning_service_account.json`
--- homeworks/1_terraform_intro/hashicorp_course/main.tf
+++ homeworks/1_terraform_intro/hashicorp_course/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("~/GCP/secrets/terraform_learning_service_account.json")
+  credentials = file(var.credentials_file)
 
-  project = "terraform-learning-349712"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
